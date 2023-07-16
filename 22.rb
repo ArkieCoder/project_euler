65af4ac976d5c927bca1304ef50fac40a43a7f1b
Jason <jhr@unitedentropy.com>
2023-07-16 11:00:09 -0500

    add code for problem 22

---
+++ 22.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+def alpha_positions(letters)
+  letters.downcase.bytes.map{|ascii| ascii - 96}
+end
+
+def name_score(name, sorted_names)
+  alpha_positions(name).inject(&:+) * (sorted_names.index(name) + 1)
+end
+
+def main
+  sorted_names = File.open("0022_names.txt").read
+    .gsub(/"/,"").split(/,/).sort
+
+  puts sorted_names.map{|name|
+    name_score(name, sorted_names)
+  }.inject(&:+)
+end
+
+main
